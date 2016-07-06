4da260722f2f0ed8522b71037f7dae0ff377d24c
OhYongTaek <ka123ak1@gmail.com>
2016-07-06 14:27:54 +0900

    rest api 사용 study 추가

---
+++ rest-express/index.js
@@ -0,0 +1,104 @@
+var hapi = require('hapi')
+  , mongoskin = require('mongoskin')
+
+var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
+
+var id = mongoskin.helper.toObjectID
+
+var server = new hapi.Server()
+server.connection({
+	host: 'localhost'
+  , port: 3000
+})
+
+var loadCollection = function (name, callback) {
+	callback(db.collection(name))
+}
+
+server.route([
+{
+	method: 'GET'
+  , path: '/'
+  , handler: function (req, reply) {
+  		reply('Select a collection, e.g., /collections/messages')
+  }
+}
+, {
+	method: 'GET'
+  , path: '/collections/{collectionName}'
+  , handler: function (req, reply) {
+  		loadCollection(req.params.collectionName, function (collection) {
+  			collection.find({}, {
+  				limit: 10
+  			  , sort: [['_id', -1]]
+  			}).toArray(function (e, results) {
+  				if (e) return reply(e)
+  				reply(results)
+  			})
+  		})
+  	}
+}
+, {
+	method: 'POST'
+  , path: '/collections/{collectionName}'
+  , handler: function (req, reply) {
+  		loadCollection(req.params.collectionName, function (collection) {
+  			collection.insert(req.payload, {}, function (e, results) {
+  				if (e) return reply(e)
+  				reply(results)
+  			})
+  		})
+  	}
+}
+, {
+	method: 'GET'
+  , path: '/collections/{collectionName}/{id}'
+  , handler: function (req, reply) {
+  		loadCollection(req.params.collectionName, function (collection) {
+  			collection.findOne({_id: id(req.params.id)}, function (e, result) {
+  				if (e) return reply(e)
+  				reply(result)
+  			})
+  		})
+  	}
+}
+, {
+	method: 'PUT'
+  , path: '/collections/{collectionName}/{id}'
+  , handler: function (req, reply) {
+  		loadCollection(req.params.collectionName, function (collection) {
+  			collection.update(
+  			{_id: id(req.params.id)}
+  		  , {$set:req.payload}
+  		  , {safe:true, multi:false}
+  		  , function (e, result) {
+  		  		if (e) return reply(e)
+  		  		reply((result.result.ok === 1) ? {msg:'success'} : {msg:'error'})
+  		  	})
+  		})
+  	}
+}
+, {
+	method: 'DELETE'
+  , path: '/collections/{collectionName}/{id}'
+  , handler: function (req, reply) {
+  		loadCollection(req.params.collectionName, function (collection) {
+  			collection.remove({_id: id(req.params.id)}, function (e, result) {
+  				if (e) return reply(e)
+  				reply((result.result.ok === 1) ? {msg:'success'} : {msg:'error'})
+  			})
+  		})
+  	}
+}
+])
+
+server.register({
+	register: require('good')
+}, (err) => {
+	if (err) {
+		return console.error(err)
+	}
+	server.start(() => {
+		console.info(`Server started at ${ server.info.uri }`);
+	})
+})
